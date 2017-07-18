e779f7c488a87c356fea04f7097ef56ee0642eeb
Eric Turner <ericturnerdev@gmail.com>
2017-07-18 12:23:46 +0900

    Return JSON

--- app.rb
+++ app.rb
@@ -5,32 +5,28 @@ require "pry"
 
 class ReflectorApp < Sinatra::Base
   get '/' do
-    set_headers
+    set_response_headers
     print_request_data
   end
 
   post "/" do
-    set_headers
+    set_response_headers
     print_request_data
   end
 
   private
 
-  def set_headers
+  def set_response_headers
     headers "Access-Control-Allow-Origin" => "*"
+    headers "Content-Type" => "application/json"
   end
 
   def print_request_data
-    <<~BODY
-      Method: #{request.env["REQUEST_METHOD"]}
-
-      --
-
-      Headers: #{JSON.pretty_generate(headers)}
-
-      --
-
-      Params: #{JSON.pretty_generate(params)}
-    BODY
+    JSON.pretty_generate({
+      method: request.env["REQUEST_METHOD"],
+      response_headers: headers,
+      params: params,
+      env: request.env
+    })
   end
 end
